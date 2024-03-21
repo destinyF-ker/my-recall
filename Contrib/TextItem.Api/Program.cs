6bdf88c8610cbe31777af1e7bfd36ead241115f0
destinyF**ker <destinyfunk@163.com>
2024-03-21 16:43:01 +0800

    Squashed commit of the following:
    commit 76a0b797ba4837df4d87da5d874dd465555d6b0f
    Author: Zhang, Yin <zhangyin@mail.neu.edu.cn>
    Date:   Wed Mar 20 20:16:38 2024 +0800
    
        NA
    
    commit 9169e314f91e05b0c683fe7ed601235f3f947ba7
    Author: Zhang, Yin <zhangyin@mail.neu.edu.cn>
    Date:   Fri Mar 15 12:29:37 2024 +0800
    
        NA
    
    commit 23025ca7c26b481ff1835971d2e87cdc2d133921
    Author: Zhang, Yin <zhangyin@mail.neu.edu.cn>
    Date:   Fri Mar 15 12:05:21 2024 +0800
    
        NA

--- Contrib/TextItem.Api/Program.cs
+++ Contrib/TextItem.Api/Program.cs
@@ -1,32 +1,25 @@
+
+using RecAll.Contrib.TextItem.Api;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.AddCustomConfiguration();
+builder.AddCustomSwagger();
+builder.AddCustomApplicationServices();
+builder.AddCustomDatabase();
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseDeveloperExceptionPage();
+    app.UseCustomSwagger();
+    app.MapGet("/", () => Results.LocalRedirect("~/swagger"));
+}
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapControllers();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+app.ApplyDatabaseMigration();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ Contrib/TextItem.Api/ProgramExtensions.cs
@@ -0,0 +1,54 @@
+using Dapr.Client;
+using Dapr.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Polly;
+using RecAll.Contrib.TextItem.Api.Services;
+
+namespace RecAll.Contrib.TextItem.Api;
+
+public static class ProgramExtensions {
+    public static void AddCustomConfiguration(
+        this WebApplicationBuilder builder) {
+        builder.Configuration.AddDaprSecretStore("recall-secretstore",
+            new DaprClientBuilder().Build());
+    }
+
+    public static void AddCustomSwagger(this WebApplicationBuilder builder) =>
+        builder.Services.AddSwaggerGen();
+
+    public static void UseCustomSwagger(this WebApplication app) {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    public static void AddCustomApplicationServices(
+        this WebApplicationBuilder builder) {
+        builder.Services.AddScoped<IIdentityService, MockIdentityService>();
+    }
+
+    public static void AddCustomDatabase(this WebApplicationBuilder builder) {
+        builder.Services.AddDbContext<TextItemContext>(p =>
+            p.UseSqlServer(
+                builder.Configuration["ConnectionStrings:TextItemContext"]));
+    }
+
+    public static void ApplyDatabaseMigration(this WebApplication app) {
+        using var scope = app.Services.CreateScope();
+
+        var retryPolicy = CreateRetryPolicy();
+        var context =
+            scope.ServiceProvider.GetRequiredService<TextItemContext>();
+
+        retryPolicy.Execute(context.Database.Migrate);
+    }
+
+    private static Policy CreateRetryPolicy() {
+        return Policy.Handle<Exception>().WaitAndRetryForever(
+            sleepDurationProvider: _ => TimeSpan.FromSeconds(5),
+            onRetry: (exception, retry, _) => {
+                Console.WriteLine(
+                    "Exception {0} with message {1} detected during database migration (retry attempt {2})",
+                    exception.GetType().Name, exception.Message, retry);
+            });
+    }
+}
